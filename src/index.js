065f39074f75ce9c33a9a7349af4d705e5116c8d
Heather Graham <heather.graham@yahoo.com>
2022-05-25 21:51:48 -0600

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-ReactDOM.render(<App />, document.getElementById('root'));
